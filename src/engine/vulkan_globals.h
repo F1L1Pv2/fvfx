b2cdac56edfd69039032da78ab2d39f457332c76
F1L1P <78918286+F1L1Pv2@users.noreply.github.com>
2025-08-20 17:06:33 +0200

    applying one vfx

---
+++ src/engine/vulkan_globals.h
@@ -0,0 +1,63 @@
+#ifndef TRIEX_VULKAN_GLOBALS
+#define TRIEX_VULKAN_GLOBALS
+
+#include "vulkan/vulkan.h"
+
+extern VkInstance instance;
+extern VkPhysicalDevice physicalDevice;
+extern VkDevice device;
+extern VkPhysicalDeviceLimits physicalDeviceLimits;
+extern VkSurfaceKHR surface;
+extern VkSwapchainKHR swapchain;
+extern VkFormat swapchainImageFormat;
+extern VkExtent2D swapchainExtent;
+extern VkQueue graphicsQueue;
+extern VkQueue presentQueue;
+extern VkPhysicalDeviceMemoryProperties memoryProperties;
+
+typedef struct {
+    VkImage* items;
+    uint32_t count;
+    size_t capacity;
+} VkImages;
+
+typedef struct {
+    VkImageView* items;
+    uint32_t count;
+    size_t capacity;
+} VkImageViews;
+
+extern VkImages swapchainImages;
+extern VkImageViews swapchainImageViews;
+
+typedef struct{
+    VkQueueFamilyProperties* items;
+    uint32_t count;
+    size_t capacity;
+} MultipleVkQueueFamilyProperties;
+extern MultipleVkQueueFamilyProperties multipleQueueFamilyProperties;
+
+extern VkCommandPool commandPool;
+extern VkCommandBuffer cmd;
+
+typedef struct {
+    float x;
+    float y;
+} vec2;
+
+typedef struct {
+    vec2 pos;
+} Vertex;
+
+extern VkPipelineLayout pipelineLayout;
+extern VkPipeline pipeline;
+
+extern VkSemaphore imageAvailableSemaphore;
+extern VkSemaphore renderFinishedSemaphore;
+extern VkFence inFlightFence;
+extern VkDescriptorPool descriptorPool;
+
+extern VkSampler samplerLinear;
+extern VkSampler samplerNearest;
+
+#endif
---
+++ src/engine/vulkan_helpers.h
@@ -0,0 +1,32 @@
+#ifndef TRIEX_VULKAN_HELPERS
+#define TRIEX_VULKAN_HELPERS
+
+#include <stdbool.h>
+
+#include "vulkan/vulkan.h"
+int getNeededQueueFamilyIndex(VkQueueFlags flags);
+bool findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties, uint32_t* index);
+VkCommandBuffer beginSingleTimeCommands();
+void endSingleTimeCommands(VkCommandBuffer commandBuffer);
+
+typedef struct {
+    float r;
+    float g;
+    float b;
+    float a;
+} Color;
+
+typedef struct{
+    VkImageView colorAttachment;
+    Color clearColor;
+    VkImageView depthAttachment;
+    VkExtent2D renderArea;
+} BeginRenderingEX;
+
+#define COL_BLACK ((Color){0.0,0.0,0.0,1.0})
+
+void vkCmdBeginRenderingEX_opt(VkCommandBuffer commandBuffer, BeginRenderingEX args);
+
+#define vkCmdBeginRenderingEX(cmd, ...) vkCmdBeginRenderingEX_opt(cmd, (BeginRenderingEX){__VA_ARGS__})
+
+#endif
